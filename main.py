7ce714dd06ad0d0f631463b60403113409341f77
Gabriel Garcia <gmgarcia1@gmail.com>
2020-07-21 22:40:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from graphics import *
+
+def main():
+  win = GraphWin('Shapes')
+    
+  center = Point(100, 100)
+  circ = Circle(center, 30)
+  circ.setFill(color_rgb(130, 0, 103))
+  circ.draw(win)
+
+  rect = Rectangle(Point(30, 30), Point(70, 70))
+  rect.setFill('blue')
+  rect.draw(win)
+
+  rect = Rectangle(Point(100, 100), Point(170, 170))
+  rect.setFill('green')
+  rect.draw(win)
+
+  win.getMouse()
+  win.close()
+
+main()
